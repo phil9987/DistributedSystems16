b36773dfba4f4e15f63d0fadfda7f928899d9d9e
David Keller <dev@davidkeller.me>
2016-10-14 18:11:27 +0200

    Add onClick handlers

--- Assignment_2/VS_kellerd_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/kellerd/vs_kellerd_webservices/MainActivity.java
+++ Assignment_2/VS_kellerd_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/kellerd/vs_kellerd_webservices/MainActivity.java
@@ -2,6 +2,7 @@ package ch.ethz.inf.vs.a2.kellerd.vs_kellerd_webservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onRESTButtonClick(View view) {
+        // TODO: Open REST Activity
+    }
+
+    public void onWSButtonClick(View view) {
+        // TODO: Open WS* Activity
+    }
+
+    public void onServerButtonClick(View view) {
+        // TODO: Open Server Activity
     }
 }

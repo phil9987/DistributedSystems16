a434492e87de81fe7012b1d0ad7f683386816e26
David Keller <dev@davidkeller.me>
2016-10-14 18:00:56 +0200

    Add Assignment 2 project

---
+++ Assignment_2/VS_kellerd_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/kellerd/vs_kellerd_webservices/MainActivity.java
@@ -0,0 +1,13 @@
+package ch.ethz.inf.vs.a2.kellerd.vs_kellerd_webservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

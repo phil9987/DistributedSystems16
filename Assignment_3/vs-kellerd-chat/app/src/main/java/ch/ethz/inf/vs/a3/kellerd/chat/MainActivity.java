38090d4a337b874cdc342c9e6393f32681e100dc
David Keller <dev@davidkeller.me>
2016-10-24 16:26:45 +0200

    Add ChatActivity & SettingsActivity

--- Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/kellerd/chat/MainActivity.java
+++ Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/kellerd/chat/MainActivity.java
@@ -1,7 +1,9 @@
 package ch.ethz.inf.vs.a3.kellerd.chat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onJoinButtonClick(View view) {
+        Intent chatIntent = new Intent(this, ChatActivity.class);
+        this.startActivity(chatIntent);
+    }
+
+    public void onSettingsButtonClick(View view) {
+        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+        this.startActivity(settingsIntent);
     }
 }

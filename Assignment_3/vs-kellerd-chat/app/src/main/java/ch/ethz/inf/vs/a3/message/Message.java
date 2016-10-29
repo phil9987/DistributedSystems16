aca9606ae83c65f4cc07a21da0f1cd3a32699949
Philip Junker <junkerp@ethz.ch>
2016-10-29 15:11:16 +0200

    Message adapted to Vector Clock, comparator adapted for vector clock comparison

--- Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/message/Message.java
+++ Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/message/Message.java
@@ -1,22 +1,27 @@
 package ch.ethz.inf.vs.a3.message;
 
+import ch.ethz.inf.vs.a3.clock.VectorClock;
+
 /**
  * Created by philipjunker on 28.10.16.
  */
 public class Message {
 
-    private String timestamp;
+    private VectorClock clock;
     private String content;
     public Message(String timestamp, String content){
-        this.timestamp = timestamp;
+        this.clock = new VectorClock();
+        clock.setClockFromString(timestamp);
         this.content = content;
     }
 
     public String getContent(){
         return content;
     }
-
+    public VectorClock getClock(){
+        return clock;
+    }
     public String getTimestamp(){
-        return timestamp;
+        return clock.toString();
     }
 }
--- Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
+++ Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
@@ -14,11 +14,13 @@ public class MessageComparator implements Comparator<Message> {
     public int compare(Message lhs, Message rhs) {
         // Returns a negative integer, zero, or a positive integer
         // as the first argument is less than, equal to, or greater than the second
-        int i1, i2;
-        i1 = Integer.parseInt(lhs.getContent().substring(lhs.getContent().length()-1));
-        i2 = Integer.parseInt(rhs.getContent().substring(rhs.getContent().length()-1));
-        //Log.d("DEBUG", "i1= " + i1 + " i2= " + i2);
-        return i1 - i2;
+        int result = 0;
+        if(lhs.getClock().happenedBefore(rhs.getClock())){
+            result = -1;
+        }else if (rhs.getClock().happenedBefore(lhs.getClock())){
+            result = 1;
+        }
+        return result;
     }
 
 }

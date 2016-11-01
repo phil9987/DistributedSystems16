3c049da65a6157aceebf9c3a18a6f07ddd9ce3d8
David Keller <dev@davidkeller.me>
2016-11-01 17:59:50 +0100

    Implement VectorClockComp & use it in MessageComp

--- Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/clock/VectorClockComparator.java
+++ Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/clock/VectorClockComparator.java
@@ -2,15 +2,21 @@ package ch.ethz.inf.vs.a3.clock;
 
 import java.util.Comparator;
 
-import ch.ethz.inf.vs.a3.solution.clock.VectorClock;
+import ch.ethz.inf.vs.a3.clock.VectorClock;
 
 
 public class VectorClockComparator implements Comparator<VectorClock> {
 
     @Override
     public int compare(VectorClock lhs, VectorClock rhs) {
-        // Write your code here
-
-        return 0;
+        // Returns a negative integer, zero, or a positive integer
+        // as the first argument is less than, equal to, or greater than the second
+        int result = 0;
+        if(lhs.happenedBefore(rhs)){
+            result = -1;
+        }else if (rhs.happenedBefore(lhs)){
+            result = 1;
+        }
+        return result;
     }
 }
--- Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
+++ Assignment_3/vs-kellerd-chat/app/src/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
@@ -3,6 +3,8 @@ package ch.ethz.inf.vs.a3.message;
 import android.util.Log;
 
 import java.util.Comparator;
+
+import ch.ethz.inf.vs.a3.clock.VectorClockComparator;
 //import ch.ethz.inf.vs.a3.solution.message.Message;
 
 /**
@@ -12,15 +14,8 @@ public class MessageComparator implements Comparator<Message> {
 
     @Override
     public int compare(Message lhs, Message rhs) {
-        // Returns a negative integer, zero, or a positive integer
-        // as the first argument is less than, equal to, or greater than the second
-        int result = 0;
-        if(lhs.getClock().happenedBefore(rhs.getClock())){
-            result = -1;
-        }else if (rhs.getClock().happenedBefore(lhs.getClock())){
-            result = 1;
-        }
-        return result;
+        VectorClockComparator vectorClockComparator = new VectorClockComparator();
+        return vectorClockComparator.compare(lhs.getClock(), rhs.getClock());
     }
 
 }
